1cc3e3dd6f2cce05860a19965defe9ceea9ff7ff
nakubman <mnakubul@ic.ac.uk>
2017-02-15 14:56:32 +0000

    Created new fucntion to plot graph

---
+++ R/descriptive_analysis_3.R
@@ -0,0 +1,12 @@
+#' plot_age_dist
+#'
+#' @param df data frame containing the data to be plotted
+#'
+#' @return plot of age distribution
+#' @export
+#'
+plot_age_dist <- function(df) {
+
+  plot(df$AgeBand)
+
+}
