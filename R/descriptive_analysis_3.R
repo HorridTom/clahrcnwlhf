df217218de155671a7a62528259f30f30139b069
nakubman <mnakubul@ic.ac.uk>
2017-04-10 14:27:00 +0100

    Added built-in addmargins function and 2, which adds a sum column for period A and B combined

--- R/descriptive_analysis_3.R
+++ R/descriptive_analysis_3.R
@@ -16,7 +16,7 @@
                                levels = c('0', '1 to 4', '5 to 9', '10 to 14', '15 to 19', '20 to 24', '25 to 29', '30 to 34', '35 to 39', '40 to 44', '45 to 49', '50 to 54', '55 to 59', '60 to 64', '65 to 69', '70 to 74', '75 to 79', '80 to 84', '85 to 89', '90 to 94', '95 to 99', '100 to 104', '105 to 109', '106 to 109', '107 to 109', '108 to 109', '109 to 109'))
 
     #plot split into periods, overall is also an option with a few edits. facet-wrap = produces the two different periods, coord_flip = flips the graphs
-    ggplot(df, aes(x=AgeBand))+geom_bar(width= 0.7)+facet_wrap( ~ period.date)+ggtitle("Distribution of age band")+coord_flip()+ylab("Count")+xlab("Age band categories")
+    ggplot(df, aes(x=AgeBand))+geom_bar(width= 0.7)+facet_wrap( ~ period.date)+ggtitle("Distribution of age band across period A and B")+coord_flip()+ylab("Frequency")+xlab("Age Band")
 
 }
 
@@ -35,9 +35,10 @@
     #stract_col name of column to split age band by
 
     #age band distribution for both periods + sum of both
-    ageband_perioddate <- table(df$AgeBand, df$period.date)
+    ageband_perioddate <- addmargins(table(df$AgeBand, df$period.date),2)
 
     ageband_perioddate #this is what I run in the command line
 
   }
+
 
