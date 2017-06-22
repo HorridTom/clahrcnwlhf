00941055e0e712dd0e60143f181d0dc9b6a9d355
HorridTom <woodcock.thomas@gmail.com>
2017-06-22 15:59:01 +0100

    Resolved conflict by incorporating both functions.

--- R/descriptive_analysis.R
+++ R/descriptive_analysis.R
@@ -417,6 +417,7 @@ los_time_table <- function(df, split_by = '%Y-%m') {
 
 }
 
+
 #' make_period_col
 #'
 #' @param df data frame to which we want to add a period column
@@ -434,4 +435,22 @@ make_period_col <- function(df, colname, split_dates, period_labels, new_colname
   bins<- as.Date(split_dates)
   df[,new_colname] <- cut(df[,colname], breaks = bins, labels = period_labels)
   df
+}
+
+#' ethn_time_table
+#'
+#' @param df a data frame of spells with column "EthnicGroup"
+#' @param split_by date format to split by
+#'
+#' @return table of counts of ethnic groups by discharge time period
+#' @export
+#'
+ethn_time_table <- function(df, split_by = '%Y-%m') {
+  df$splitby <- factor(format(df$DischargeDate, split_by))
+  #df_splt <- split.data.frame(df, df$splitby)
+
+  m <- table(df$EthnicGroup,df$splitby)
+
+  m
+
 }
