92339cb608f45fb54a8742a561e15598cd976b91
HorridTom <woodcock.thomas@gmail.com>
2017-06-22 16:09:39 +0100

    Resolved conflict - incorporated both changes.

--- R/descriptive_analysis.R
+++ R/descriptive_analysis.R
@@ -474,3 +474,22 @@ ethn_time_table <- function(df, split_by = '%Y-%m', ethn_col = 'EthnicGroup') {
 }
 
 
+#' make_period_col
+#'
+#' @param df data frame to which we want to add a period column
+#' @param colname column in df containing all the dates on which the period established
+#' @param split_dates vector of strings representing the date cut-offs of the periods.
+#' Note the first entry must a date before the earliest date in colname
+#' and the last date must be a date after the final date in colname
+#' @param period_labels labels for periods split by split_dates
+#' @param new_colname name of new column created containing the period into which the date falls
+#'
+#' @return df with an extra column of periods
+#' @export
+#'
+make_period_col <- function(df, colname, split_dates, period_labels, new_colname) {
+  bins<- as.Date(split_dates)
+  df[,new_colname] <- cut(df[,colname], breaks = bins, labels = period_labels)
+  df
+}
+
