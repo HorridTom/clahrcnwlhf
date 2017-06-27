5ea13aa550220fd4b88715f147ecf75ca6cf1263
HorridTom <woodcock.thomas@gmail.com>
2017-06-27 10:54:10 +0100

    Updated documentation and age analysis vignette.

--- R/descriptive_analysis_3.R
+++ R/descriptive_analysis_3.R
@@ -1,53 +1,37 @@
 
 #' plot_age_dist
 #'
-#'  @param df data frame containing the data to be plotted
-#'
-#'  @return plot of age distribution
-#'  @export
-#'
-    plot_age_dist <- function(df) {
-
-      #dataframe restricted to only rows where 'new.spell' = TRUE
-    df <- restrict_to_spells(df)
-
-      #plot graph split into period.adte A and B, overall is also an option with a few edits. facet-wrap = produces the two different periods, coord_flip = flips the graphs
-   ggplot2::ggplot(df, ggplot2::aes(x=AgeBand))+ggplot2::geom_bar(width= 0.7)+ggplot2::facet_wrap( ~ period.date)+ggplot2::ggtitle("Distribution by Age Bands Across Period Dates A and B")+ggplot2::coord_flip()+ggplot2::ylab("Frequency")+ggplot2::xlab("Age Band")
-
-}
-
-
-
-#' test_plot_age_dist
-#'
 #' @param df data frame containing the data to be plotted
 #'
 #' @return plot of age distribution
 #' @export
 #'
-
-    test_plot_age_dist <- function(df) {
+    plot_age_dist <- function(df, stratify = FALSE) {
 
       #dataframe restricted to only rows where 'new.spell' = TRUE
-      df <- restrict_to_spells(df)
+    df <- clahrcnwlhf::restrict_to_spells(df)
 
-      #plot graph for age band split by period.date A and B, overall is also an option with a few edits. facet-wrap = produces the two different periods, coord_flip = flips the graphs
-      if (facet_wrap) #(specified/split for period.date A and B)
-        {
-        ggplot2::ggplot(df, ggplot2::aes(x=AgeBand))+ggplot2::geom_bar(width= 0.7)+ggplot2::facet_wrap( ~ period.date)+ggplot2::ggtitle("Distribution by Age Bands Across Period Dates A and B")+ggplot2::coord_flip()+ggplot2::ylab("Frequency")+ggplot2::xlab("Age Band")
-        } else {  #(overall distribution of age band i.e period.date A & B combined)
-          ggplot2::ggplot(df, ggplot2::aes(x=AgeBand))+ggplot2::geom_bar(width= 0.7)+ggplot2::ggtitle("Distribution by Age Bands")+ggplot2::coord_flip()+ggplot2::ylab("Frequency")+ggplot2::xlab("Age Band")
-        }
+      #plot graph split into period.adte A and B, overall is also an option with a few edits. facet-wrap = produces the two different periods, coord_flip = flips the graphs
+   age_plot <- ggplot2::ggplot(df, ggplot2::aes(x=AgeBand))+ggplot2::geom_bar(width= 0.7)+ggplot2::coord_flip()+ggplot2::ylab("Frequency")+ggplot2::xlab("Age Band")
+
+   if (stratify) {
+     age_plot <- age_plot +ggplot2::facet_wrap( ~ period.date) + ggplot2::ggtitle("Distribution by Age Bands Across Period Dates A and B")
+   } else {
+     age_plot <- age_plot + ggplot2::ggtitle("Distribution by Age Bands")
+   }
+   age_plot
 }
+
+
 #' plot_dist_table
 #'
-#'  @param df dataframe
-#'  @param age_col column containing age band variable
-#'  @param stratify indicate whether table is split into periods or overall
-#'  @param strat_col name of column to split age band by
+#' @param df dataframe
+#' @param age_col column containing age band variable
+#' @param stratify indicate whether table is split into periods or overall
+#' @param strat_col name of column to split age band by
 #'
-#'  @return a (two-way) summary table of age band distribution containing frequncies for each period.date and total sums for both periods
-#'  @export
+#' @return a (two-way) summary table of age band distribution containing frequncies for each period.date and total sums for both periods
+#' @export
 #'
     plot_dist_table <- function(df, age_col = 'AgeBand', stratify = FALSE, strat_col){
 
