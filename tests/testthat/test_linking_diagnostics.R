da3e5b798536b2d7e76d6a13c5e4e606153e0dd6
Woodcock <tw299@ic.ac.uk>
2017-06-30 13:07:01 +0100

    Renamed prev_spell to nearest_spells, and added failing assertions to test addition of next.spell column by nearest_spells.

--- tests/testthat/test_linking_diagnostics.R
+++ tests/testthat/test_linking_diagnostics.R
@@ -1,7 +1,7 @@
 library(clahrcnwlhf)
 context("Linking diagnostics")
 
-test_that("prev.spell column is correctly created by prev_spell",{
+test_that("prev.spell and next.spell columns are correctly created by nearest_spells",{
 
   load("datafortesting/bundle_link_test_data.Rda")
 
@@ -9,14 +9,23 @@ test_that("prev.spell column is correctly created by prev_spell",{
   # in test_bundle_linking.R
   correct_results <- bundle_link_test_data
   correct_results$prev.spell <- NA
+  correct_results$next.spell <- NA
   correct_results[correct_results$PseudoID == 1217659, "prev.spell"] <- 179895
   correct_results[correct_results$PseudoID == 1132437, "prev.spell"] <- 108960
+  correct_results[correct_results$PseudoID == 1217659, "next.spell"] <- 179896
+  correct_results[correct_results$PseudoID == 1132437, "next.spell"] <- 108961
 
-  bundles_with_prev_spell <- prev_spell(bundles = bundle_link_test_data, episodes = clahrcnwlhf::emergency_adms)
+  bundles_with_nearest_spells <- nearest_spells(bundles = bundle_link_test_data, episodes = clahrcnwlhf::emergency_adms)
 
-  expect_match(colnames(bundles_with_prev_spell), "prev.spell", all = FALSE)
+  # Check the output columns exist
+  expect_match(colnames(bundles_with_nearest_spells), "prev.spell", all = FALSE)
+  expect_match(colnames(bundles_with_nearest_spells), "next.spell", all = FALSE)
 
-  expect_equal(bundles_with_prev_spell[bundles_with_prev_spell$PseudoID == 1217659, "prev.spell"], correct_results[correct_results$PseudoID == 1217659, "prev.spell"])
-  expect_equal(bundles_with_prev_spell[bundles_with_prev_spell$PseudoID == 1132437, "prev.spell"], correct_results[correct_results$PseudoID == 1132437, "prev.spell"])
+  # Check the values returned in these columns are correct
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1217659, "prev.spell"], correct_results[correct_results$PseudoID == 1217659, "prev.spell"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1132437, "prev.spell"], correct_results[correct_results$PseudoID == 1132437, "prev.spell"])
+
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1217659, "next.spell"], correct_results[correct_results$PseudoID == 1217659, "next.spell"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1132437, "next.spell"], correct_results[correct_results$PseudoID == 1132437, "next.spell"])
 
 })
