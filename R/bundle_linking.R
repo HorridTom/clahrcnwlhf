85678817f38e6fe71733eb6274abc6e8e99e565d
HorridTom <woodcock.thomas@gmail.com>
2017-06-30 12:24:05 +0100

    Test passing: prev.spell column is correctly created by prev_spell

--- R/bundle_linking.R
+++ R/bundle_linking.R
@@ -3,7 +3,7 @@
 #' @param bundles the bundle data
 #' @param episodes the episode data
 #'
-#' @return dataframe with new column linked.spell containing spellID of the spell associated with this bundle
+#' @return dataframe with new column linked.spell containing spell_number of the spell associated with this bundle
 #' @export
 #'
 link_bundles <- function(bundles, episodes) {
@@ -13,6 +13,17 @@ link_bundles <- function(bundles, episodes) {
 
 
 prev_spell <- function(bundles, episodes) {
-  bundles$prev.spell <- NA
+
+
+
+  bundles$prev.spell <- apply(bundles, 1, function(x) {
+    pt_id <- x["PseudoID"]
+    bun_dt <- x["Admission.Datetime"]
+
+    pt_eps <- episodes[episodes$PseudoID == pt_id,]
+    pt_eps <- pt_eps[as.POSIXct(pt_eps$CSPAdmissionTime) <= as.POSIXct(bun_dt),]
+
+    pt_eps[which.max(as.POSIXct(pt_eps$CSPAdmissionTime)),"spell_number"]
+  })
   bundles
 }
