6b7593587423bc082ebe47789165c0fca369daf4
Woodcock <tw299@ic.ac.uk>
2017-06-30 13:11:36 +0100

    Test passing: prev.spell and next.spell columns are correctly created by nearest_spells.

--- R/bundle_linking.R
+++ R/bundle_linking.R
@@ -14,8 +14,6 @@ link_bundles <- function(bundles, episodes) {
 
 nearest_spells <- function(bundles, episodes) {
 
-
-
   bundles$prev.spell <- apply(bundles, 1, function(x) {
 
     # Extract the patient id and admission datetime for this bundle
@@ -30,9 +28,19 @@ nearest_spells <- function(bundles, episodes) {
     pt_eps[which.max(as.POSIXct(pt_eps$CSPAdmissionTime)),"spell_number"]
   })
 
+  bundles$next.spell <- apply(bundles, 1, function(x) {
 
+    # Extract the patient id and admission datetime for this bundle
+    pt_id <- x["PseudoID"]
+    bun_dt <- x["Admission.Datetime"]
 
+    # Identify the first subsequent admission for this patient
+    # after the bundle admission datetime
+    pt_eps <- episodes[episodes$PseudoID == pt_id,]
+    pt_eps <- pt_eps[as.POSIXct(pt_eps$CSPAdmissionTime) > as.POSIXct(bun_dt),]
 
+    pt_eps[which.min(as.POSIXct(pt_eps$CSPAdmissionTime)),"spell_number"]
+  })
 
   bundles
 }
