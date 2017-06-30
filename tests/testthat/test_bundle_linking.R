d826b7fd24466ab93aa99df4bb281a2649c3f6a0
HorridTom <woodcock.thomas@gmail.com>
2017-06-30 11:02:11 +0100

    Added test for presence of linking column in result of link_bundles

---
+++ tests/testthat/test_bundle_linking.R
@@ -0,0 +1,19 @@
+library(clahrcnwlhf)
+context("Bundle linking")
+
+create_bundle_link_test_dataset <- function() {
+
+  bundle_link_cols <- c("Bundle.Number","PseudoID","Admission.Datetime")
+  bundle_link_test_rows <- c(1,2)
+  bundle_link_test_data <- clahrcnwlhf::bundle_data_clean[bundle_link_test_rows, which(colnames(clahrcnwlhf::bundle_data_clean) %in% bundle_link_cols)]
+  save(bundle_link_test_data, file="tests/testthat/datafortesting/bundle_link_test_data.Rda")
+}
+
+
+test_that("linked.episode column is created by link_bundles",{
+
+  load("datafortesting/bundle_link_test_data.Rda")
+  linked_bundles <- link_bundles(bundles = bundle_link_test_data, episodes = clahrcnwl::emergency_adms)
+  expect_match(colnames(linked_bundles), "linked.episode")
+
+})
