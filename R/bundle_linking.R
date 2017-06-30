27e361fa881501d7e1a43cdde308a6ef06cdc8e1
Woodcock <tw299@ic.ac.uk>
2017-06-30 13:07:01 +0100

    Renamed prev_spell to nearest_spells, and added failing assertions to test addition of next.spell column by nearest_spells.

--- R/bundle_linking.R
+++ R/bundle_linking.R
@@ -12,18 +12,27 @@ link_bundles <- function(bundles, episodes) {
 }
 
 
-prev_spell <- function(bundles, episodes) {
+nearest_spells <- function(bundles, episodes) {
 
 
 
   bundles$prev.spell <- apply(bundles, 1, function(x) {
+
+    # Extract the patient id and admission datetime for this bundle
     pt_id <- x["PseudoID"]
     bun_dt <- x["Admission.Datetime"]
 
+    # Identify the most recent admission for this patient
+    # prior to or on the bundle admission datetime
     pt_eps <- episodes[episodes$PseudoID == pt_id,]
     pt_eps <- pt_eps[as.POSIXct(pt_eps$CSPAdmissionTime) <= as.POSIXct(bun_dt),]
 
     pt_eps[which.max(as.POSIXct(pt_eps$CSPAdmissionTime)),"spell_number"]
   })
+
+
+
+
+
   bundles
 }
