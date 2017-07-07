21c0d4bc4a1df4b6908a61b8adf264e5c9e612af
Woodcock <tw299@ic.ac.uk>
2017-07-07 15:48:43 +0100

    Fixed problem with nearest spell identification, with test.
    Also added code to plot charts and table for diagnostics.
    
    Problem was unit mixup in calculating time differences.

--- tests/testthat/test_bundle_linking.R
+++ tests/testthat/test_bundle_linking.R
@@ -16,4 +16,13 @@ test_that("linked.spell column is created by link_bundles",{
   linked_bundles <- link_bundles(bundles = bundle_link_test_data, episodes = clahrcnwl::emergency_adms)
   expect_match(colnames(linked_bundles), "linked.spell", all = FALSE)
 
+  # Best linking algorithm idea at present:
+  # 1. Any bundle with bundle.in.spell == TRUE should be linked to its prev.spell (320)
+  # 2. Of the rest (449), any bundle with lag.to.next.adm <= 3 days (361) should be linked to its next.spell
+  # 3. This leaves:
+  #   a) 11 bundles not in spell with lag.to.next.adm > 3 days
+  #   b) 77 bundles not in spell with no subsequent admission. The distribution of lag.from.prev.adm
+  #       for this subset shows that they are not close in time to the previous admission.
+  # These 88 bundles under (3) require further investigation.
+
 })
