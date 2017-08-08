bbd6429c60a6f07760c4cfa33d67f305900e8951
HorridTom <woodcock.thomas@gmail.com>
2017-08-08 12:36:15 +0100

    Function to remove duplicated bundle links, with test and minimal documentation.

--- tests/testthat/test_bundle_linking.R
+++ tests/testthat/test_bundle_linking.R
@@ -10,6 +10,22 @@ create_bundle_link_test_dataset <- function() {
 }
 
 
+create_bundle_link_dupe_removal_test_dataset <- function() {
+
+  bundle_dupe_link_cols <- c("Bundle.Number","PseudoID","Admission.Datetime",
+                             "number.nas")
+  bundle_dupe_link_bundle_numbers <- c(192, 196, 193, 198, 591, 698, 207, 213,
+                                       234)
+
+  bundle_link_dupe_removal_test_data <-
+    clahrcnwlhf::bundle_data_clean[which(clahrcnwlhf::bundle_data_clean$Bundle.Number %in% bundle_dupe_link_bundle_numbers),
+                                   which(colnames(clahrcnwlhf::bundle_data_clean) %in% bundle_dupe_link_cols)]
+  save(bundle_link_dupe_removal_test_data,
+       file="tests/testthat/datafortesting/bundle_link_dupe_removal_test_data.Rda")
+
+}
+
+
 create_nicor_link_test_dataset <- function() {
 
   nicor_link_cols <- c("PseudoID","Date.of.Visit")
@@ -89,5 +105,41 @@ test_that("linked.spell column is created by link_nicor",{
   expect_equal(linked_nicor[linked_nicor$PseudoID == 1203126, "linked.spell"], correct_results[correct_results$PseudoID == 1203126, "linked.spell"])
   expect_equal(linked_nicor[linked_nicor$PseudoID == 2000010, "linked.spell"], correct_results[correct_results$PseudoID == 2000010, "linked.spell"])
 
+
+})
+
+
+test_that("Duplicate links are removed correctly", {
+
+  load("datafortesting/bundle_link_dupe_removal_test_data.Rda")
+  linked_bundles <- link_bundles(bundles = bundle_link_dupe_removal_test_data,
+                                 episodes = clahrcnwlhf::emergency_adms)
+
+  # Specify correct results
+  correct_results <- bundle_link_dupe_removal_test_data[
+    which(bundle_link_dupe_removal_test_data$Bundle.Number %in% c(196, 198, 591, 207)),]
+  correct_results$linked.spell <- NA
+
+  correct_results[correct_results$Bundle.Number == 196, "linked.spell"] <- 8223
+  correct_results[correct_results$Bundle.Number == 198, "linked.spell"] <- 13473
+  correct_results[correct_results$Bundle.Number == 591, "linked.spell"] <- 51984
+  correct_results[correct_results$Bundle.Number == 207, "linked.spell"] <- 147843
+
+  # Run the duplicate link removal function
+  deduped_linked_bundles <- remove_duplicate_bundle_links(linked_bundles = linked_bundles)
+
+  # Test that the correct bundles are linked to each spell
+  expect_equal(deduped_linked_bundles[deduped_linked_bundles$Bundle.Number == 196, "linked.spell"],
+               correct_results[correct_results$Bundle.Number == 196, "linked.spell"])
+  expect_equal(deduped_linked_bundles[deduped_linked_bundles$Bundle.Number == 198, "linked.spell"],
+               correct_results[correct_results$Bundle.Number == 198, "linked.spell"])
+  expect_equal(deduped_linked_bundles[deduped_linked_bundles$Bundle.Number == 591, "linked.spell"],
+               correct_results[correct_results$Bundle.Number == 591, "linked.spell"])
+  expect_equal(deduped_linked_bundles[deduped_linked_bundles$Bundle.Number == 207, "linked.spell"],
+               correct_results[correct_results$Bundle.Number == 207, "linked.spell"])
+
+  #Test that each spell only appears once in the deduped data
+  expect_equal(length(deduped_linked_bundles$linked.spell),
+               length(unique(deduped_linked_bundles$linked.spell)))
 
 })
