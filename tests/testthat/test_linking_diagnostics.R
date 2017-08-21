24af89d5f1de0f7f802c3a295f8c29a35dcdd94b
HorridTom <woodcock.thomas@gmail.com>
2017-08-21 13:06:28 +0100

    Refactored tests to use unique ID for bundles/nicor records rather than patient PseudoID.

--- tests/testthat/test_linking_diagnostics.R
+++ tests/testthat/test_linking_diagnostics.R
@@ -11,15 +11,15 @@ test_that("prev.spell and next.spell columns are correctly created by nearest_sp
   correct_results$prev.spell <- NA
   correct_results$next.spell <- NA
 
-  correct_results[correct_results$PseudoID == 1217659, "prev.spell"] <- 179895
-  correct_results[correct_results$PseudoID == 1132437, "prev.spell"] <- 108960
-  correct_results[correct_results$PseudoID == 1241298, "prev.spell"] <- 199432
-  correct_results[correct_results$PseudoID == 1062983, "prev.spell"] <- NA
+  correct_results[correct_results$Bundle.Number == 1, "prev.spell"] <- 179895
+  correct_results[correct_results$Bundle.Number == 2, "prev.spell"] <- 108960
+  correct_results[correct_results$Bundle.Number == 3, "prev.spell"] <- 199432
+  correct_results[correct_results$Bundle.Number == 31, "prev.spell"] <- NA
 
-  correct_results[correct_results$PseudoID == 1217659, "next.spell"] <- 179896
-  correct_results[correct_results$PseudoID == 1132437, "next.spell"] <- 108961
-  correct_results[correct_results$PseudoID == 1241298, "next.spell"] <- NA
-  correct_results[correct_results$PseudoID == 1062983, "next.spell"] <- 51934
+  correct_results[correct_results$Bundle.Number == 1, "next.spell"] <- 179896
+  correct_results[correct_results$Bundle.Number == 2, "next.spell"] <- 108961
+  correct_results[correct_results$Bundle.Number == 3, "next.spell"] <- NA
+  correct_results[correct_results$Bundle.Number == 31, "next.spell"] <- 51934
 
   bundles_with_nearest_spells <- nearest_spells(bundles = bundle_link_test_data, episodes = clahrcnwlhf::emergency_adms)
 
@@ -28,15 +28,15 @@ test_that("prev.spell and next.spell columns are correctly created by nearest_sp
   expect_match(colnames(bundles_with_nearest_spells), "next.spell", all = FALSE)
 
   # Check the values returned in these columns are correct
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1217659, "prev.spell"], correct_results[correct_results$PseudoID == 1217659, "prev.spell"])
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1132437, "prev.spell"], correct_results[correct_results$PseudoID == 1132437, "prev.spell"])
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1241298, "prev.spell"], correct_results[correct_results$PseudoID == 1241298, "prev.spell"])
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1062983, "prev.spell"], correct_results[correct_results$PseudoID == 1062983, "prev.spell"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 1, "prev.spell"], correct_results[correct_results$Bundle.Number == 1, "prev.spell"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 2, "prev.spell"], correct_results[correct_results$Bundle.Number == 2, "prev.spell"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 3, "prev.spell"], correct_results[correct_results$Bundle.Number == 3, "prev.spell"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 31, "prev.spell"], correct_results[correct_results$Bundle.Number == 31, "prev.spell"])
 
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1217659, "next.spell"], correct_results[correct_results$PseudoID == 1217659, "next.spell"])
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1132437, "next.spell"], correct_results[correct_results$PseudoID == 1132437, "next.spell"])
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1241298, "next.spell"], correct_results[correct_results$PseudoID == 1241298, "next.spell"])
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1062983, "next.spell"], correct_results[correct_results$PseudoID == 1062983, "next.spell"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 1, "next.spell"], correct_results[correct_results$Bundle.Number == 1, "next.spell"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 2, "next.spell"], correct_results[correct_results$Bundle.Number == 2, "next.spell"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 3, "next.spell"], correct_results[correct_results$Bundle.Number == 3, "next.spell"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 31, "next.spell"], correct_results[correct_results$Bundle.Number == 31, "next.spell"])
 
 })
 
@@ -49,11 +49,11 @@ test_that("bundle.in.spell column is correctly created by bundle_in_spell",{
   # in test_bundle_linking.R
   correct_results <- bundle_link_test_data
   correct_results$bundle.in.spell <- NA
-  correct_results[correct_results$PseudoID == 1217659, "bundle.in.spell"] <- TRUE
-  correct_results[correct_results$PseudoID == 1132437, "bundle.in.spell"] <- TRUE
-  correct_results[correct_results$PseudoID == 1241298, "bundle.in.spell"] <- TRUE
-  correct_results[correct_results$PseudoID == 1062983, "bundle.in.spell"] <- NA
-  correct_results[correct_results$PseudoID == 1076292, "bundle.in.spell"] <- FALSE
+  correct_results[correct_results$Bundle.Number == 1, "bundle.in.spell"] <- TRUE
+  correct_results[correct_results$Bundle.Number == 2, "bundle.in.spell"] <- TRUE
+  correct_results[correct_results$Bundle.Number == 3, "bundle.in.spell"] <- TRUE
+  correct_results[correct_results$Bundle.Number == 31, "bundle.in.spell"] <- NA
+  correct_results[correct_results$Bundle.Number == 5, "bundle.in.spell"] <- FALSE
 
   # Run the bundle_in_spell function
   bundles_with_in_spell <- bundle_in_spell(bundle_link_test_data)
@@ -62,11 +62,11 @@ test_that("bundle.in.spell column is correctly created by bundle_in_spell",{
   expect_match(colnames(bundles_with_in_spell), "bundle.in.spell", all = FALSE)
 
   # Check the values returned in these columns are correct
-  expect_equal(bundles_with_in_spell[bundles_with_in_spell$PseudoID == 1217659, "bundle.in.spell"], correct_results[correct_results$PseudoID == 1217659, "bundle.in.spell"])
-  expect_equal(bundles_with_in_spell[bundles_with_in_spell$PseudoID == 1132437, "bundle.in.spell"], correct_results[correct_results$PseudoID == 1132437, "bundle.in.spell"])
-  expect_equal(bundles_with_in_spell[bundles_with_in_spell$PseudoID == 1241298, "bundle.in.spell"], correct_results[correct_results$PseudoID == 1241298, "bundle.in.spell"])
-  expect_equal(bundles_with_in_spell[bundles_with_in_spell$PseudoID == 1062983, "bundle.in.spell"], correct_results[correct_results$PseudoID == 1062983, "bundle.in.spell"])
-  expect_equal(bundles_with_in_spell[bundles_with_in_spell$PseudoID == 1076292, "bundle.in.spell"], correct_results[correct_results$PseudoID == 1076292, "bundle.in.spell"])
+  expect_equal(bundles_with_in_spell[bundles_with_in_spell$Bundle.Number == 1, "bundle.in.spell"], correct_results[correct_results$Bundle.Number == 1, "bundle.in.spell"])
+  expect_equal(bundles_with_in_spell[bundles_with_in_spell$Bundle.Number == 2, "bundle.in.spell"], correct_results[correct_results$Bundle.Number == 2, "bundle.in.spell"])
+  expect_equal(bundles_with_in_spell[bundles_with_in_spell$Bundle.Number == 3, "bundle.in.spell"], correct_results[correct_results$Bundle.Number == 3, "bundle.in.spell"])
+  expect_equal(bundles_with_in_spell[bundles_with_in_spell$Bundle.Number == 31, "bundle.in.spell"], correct_results[correct_results$Bundle.Number == 31, "bundle.in.spell"])
+  expect_equal(bundles_with_in_spell[bundles_with_in_spell$Bundle.Number == 5, "bundle.in.spell"], correct_results[correct_results$Bundle.Number == 5, "bundle.in.spell"])
 
 })
 
@@ -80,13 +80,13 @@ test_that("nearest_spells function returns lag columns",{
   correct_results$lag.from.prev.adm <- NA
   correct_results$lag.to.next.adm <- NA
 
-  correct_results[correct_results$PseudoID == 1217659, "lag.from.prev.adm"] <- as.difftime(2, units = "days")
-  correct_results[correct_results$PseudoID == 1062983, "lag.from.prev.adm"] <- NA
+  correct_results[correct_results$Bundle.Number == 1, "lag.from.prev.adm"] <- as.difftime(2, units = "days")
+  correct_results[correct_results$Bundle.Number == 31, "lag.from.prev.adm"] <- NA
   correct_results$lag.from.prev.adm <- as.difftime(correct_results$lag.from.prev.adm, units = "days")
 
-  correct_results[correct_results$PseudoID == 1217659, "lag.to.next.adm"] <- as.difftime(31.52431, units = "days")
-  correct_results[correct_results$PseudoID == 1241298, "lag.to.next.adm"] <- NA
-  correct_results[correct_results$PseudoID == 1076292, "lag.to.next.adm"] <- as.difftime(45/(60*24), units = "days")
+  correct_results[correct_results$Bundle.Number == 1, "lag.to.next.adm"] <- as.difftime(31.52431, units = "days")
+  correct_results[correct_results$Bundle.Number == 3, "lag.to.next.adm"] <- NA
+  correct_results[correct_results$Bundle.Number == 5, "lag.to.next.adm"] <- as.difftime(45/(60*24), units = "days")
   correct_results$lag.to.next.adm <- as.difftime(correct_results$lag.to.next.adm, units = "days")
 
   # Run the function
@@ -100,13 +100,13 @@ test_that("nearest_spells function returns lag columns",{
   expect_match(class(bundles_with_nearest_spells$lag.to.next.adm),"difftime")
 
   # TODO: Write expectations on results here
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1217659, "lag.from.prev.adm"], correct_results[correct_results$PseudoID == 1217659, "lag.from.prev.adm"])
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1062983, "lag.from.prev.adm"], correct_results[correct_results$PseudoID == 1062983, "lag.from.prev.adm"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 1, "lag.from.prev.adm"], correct_results[correct_results$Bundle.Number == 1, "lag.from.prev.adm"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 31, "lag.from.prev.adm"], correct_results[correct_results$Bundle.Number == 31, "lag.from.prev.adm"])
 
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1217659, "lag.to.next.adm"], correct_results[correct_results$PseudoID == 1217659, "lag.to.next.adm"], tolerance = 1e-4)
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1241298, "lag.to.next.adm"], correct_results[correct_results$PseudoID == 1241298, "lag.to.next.adm"], tolerance = 1e-4)
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1076292, "lag.to.next.adm"], correct_results[correct_results$PseudoID == 1076292, "lag.to.next.adm"], tolerance = 1e-4,
-               info = paste(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1076292, "lag.to.next.adm"],correct_results[correct_results$PseudoID == 1076292, "lag.to.next.adm"], sep = "---"))
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 1, "lag.to.next.adm"], correct_results[correct_results$Bundle.Number == 1, "lag.to.next.adm"], tolerance = 1e-4)
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 3, "lag.to.next.adm"], correct_results[correct_results$Bundle.Number == 3, "lag.to.next.adm"], tolerance = 1e-4)
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 5, "lag.to.next.adm"], correct_results[correct_results$Bundle.Number == 5, "lag.to.next.adm"], tolerance = 1e-4,
+               info = paste(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 5, "lag.to.next.adm"],correct_results[correct_results$Bundle.Number == 5, "lag.to.next.adm"], sep = "---"))
 
 
 })
@@ -121,11 +121,11 @@ test_that("nearest_spells pulls in admission dates for prev.spell and next.spell
   #correct_results$prev.adm.dt <- strptime(NA, format = "%Y-%m-%d %H:%M:%S")
   #correct_results$next.adm.dt <- strptime(NA, format = "%Y-%m-%d %H:%M:%S")
 
-  correct_results[correct_results$PseudoID == 1217659, "prev.adm.dt"] <- as.POSIXct(strptime("2015-07-25 14:45:00", format = "%Y-%m-%d %H:%M:%S"))
-  correct_results[correct_results$PseudoID == 1062983, "prev.adm.dt"] <- as.POSIXct(strptime(NA, format = "%Y-%m-%d %H:%M:%S"))
+  correct_results[correct_results$Bundle.Number == 1, "prev.adm.dt"] <- as.POSIXct(strptime("2015-07-25 14:45:00", format = "%Y-%m-%d %H:%M:%S"))
+  correct_results[correct_results$Bundle.Number == 31, "prev.adm.dt"] <- as.POSIXct(strptime(NA, format = "%Y-%m-%d %H:%M:%S"))
 
-  correct_results[correct_results$PseudoID == 1217659, "next.adm.dt"] <- as.POSIXct(strptime("2015-08-28 03:20:00", format = "%Y-%m-%d %H:%M:%S"))
-  correct_results[correct_results$PseudoID == 1241298, "next.adm.dt"] <- as.POSIXct(strptime(NA, format = "%Y-%m-%d %H:%M:%S"))
+  correct_results[correct_results$Bundle.Number == 1, "next.adm.dt"] <- as.POSIXct(strptime("2015-08-28 03:20:00", format = "%Y-%m-%d %H:%M:%S"))
+  correct_results[correct_results$Bundle.Number == 3, "next.adm.dt"] <- as.POSIXct(strptime(NA, format = "%Y-%m-%d %H:%M:%S"))
 
   correct_results$prev.adm.dt <- as.POSIXct(correct_results$prev.adm.dt, origin="1970-01-01")
   correct_results$next.adm.dt <- as.POSIXct(correct_results$next.adm.dt, origin="1970-01-01")
@@ -138,10 +138,10 @@ test_that("nearest_spells pulls in admission dates for prev.spell and next.spell
   expect_match(colnames(bundles_with_nearest_spells), "next.adm.dt", all = FALSE)
 
   # Check results are correct
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1217659, "prev.adm.dt"], correct_results[correct_results$PseudoID == 1217659, "prev.adm.dt"])
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1062983, "prev.adm.dt"], correct_results[correct_results$PseudoID == 1062983, "prev.adm.dt"])
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1217659, "next.adm.dt"], correct_results[correct_results$PseudoID == 1217659, "next.adm.dt"])
-  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$PseudoID == 1241298, "next.adm.dt"], correct_results[correct_results$PseudoID == 1241298, "next.adm.dt"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 1, "prev.adm.dt"], correct_results[correct_results$Bundle.Number == 1, "prev.adm.dt"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 31, "prev.adm.dt"], correct_results[correct_results$Bundle.Number == 31, "prev.adm.dt"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 1, "next.adm.dt"], correct_results[correct_results$Bundle.Number == 1, "next.adm.dt"])
+  expect_equal(bundles_with_nearest_spells[bundles_with_nearest_spells$Bundle.Number == 3, "next.adm.dt"], correct_results[correct_results$Bundle.Number == 3, "next.adm.dt"])
 
 })
 
@@ -179,18 +179,22 @@ test_that("plot_linking_venn generates a venn diagram showing linkage results", 
   load("datafortesting/bundle_link_test_data.Rda")
   load("datafortesting/nicor_link_test_data.Rda")
 
+  bundle_venn_test_bundle_numbers <- c(1,2,3,31,5,141,439, 152)
+  nicor_venn_test_entries <- c(1,1149,72,914,150,604)
+
   linked_bundles <- link_bundles(bundles = bundle_link_test_data)
   linked_nicor <- link_nicor(nicor = nicor_link_test_data)
 
   venn.d <- plot_linking_venn(episodes = clahrcnwlhf::emergency_adms,
-                              linked_bundles = linked_bundles[c(1:8),],
-                              linked_nicor = linked_nicor, plot_vd = FALSE)
+                              linked_bundles = linked_bundles[which(linked_bundles$Bundle.Number %in% bundle_venn_test_bundle_numbers),],
+                              linked_nicor = linked_nicor[which(linked_nicor$nicor.entry.id %in% nicor_venn_test_entries),],
+                              plot_vd = FALSE)
 
   expect_is(venn.d[[1]], "polygon")
   expect_is(venn.d[[length(venn.d)]],"text")
 
-  expect_equal(venn.d[[7]]$label, "2")
-  expect_equal(venn.d[[8]]$label, "263246")
+  expect_equal(venn.d[[7]]$label, "3")
+  expect_equal(venn.d[[8]]$label, "263245")
   expect_equal(venn.d[[9]]$label, "5")
 
 })
