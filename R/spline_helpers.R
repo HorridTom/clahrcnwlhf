0e9338685f515c5c0172d544952566fef6bfd84a
HorridTom <woodcock.thomas@gmail.com>
2023-05-26 14:10:41 +0100

    Functions to derive slope coefficients pre- and post-intervention from one-knot linear spline regression.

---
+++ R/spline_helpers.R
@@ -0,0 +1,103 @@
+# Spline helper functions
+
+get_basis_functions_for_linear_spline_with_one_knot <- function(spline_basis) {
+
+  knots <- attr(spline_basis, "knots")
+  boundary_knots <- attr(spline_basis, "Boundary.knots")
+
+  spline_basis_df <- as_tibble(spline_basis) %>%
+    mutate(x = row_number()) %>%
+    pivot_longer(cols = -x) %>%
+    mutate(value = as.numeric(value))
+
+  basis_fn_names <- spline_basis_df %>%
+    distinct(name) %>%
+    pull(name)
+
+  spline_basis_list <- list()
+
+  for(basis_fn_name in basis_fn_names) {
+    basis_fn_df <- spline_basis_df %>%
+      filter(name == basis_fn_name)
+
+    # Segment 1
+    x1 <- boundary_knots[1]
+    x2 <- knots[1]
+    y1 <- basis_fn_df %>%
+      filter(x == x1) %>%
+      pull(value)
+    y2 <- basis_fn_df %>%
+      filter(x == x2) %>%
+      pull(value)
+
+    seg1_m <- (y2 - y1) / (x2 - x1)
+    seg1_c <- (x2*y1 - x1*y2) / (x2 - x1)
+
+    # Segment 2
+    x1 <- knots[1]
+    x2 <- boundary_knots[2]
+    y1 <- basis_fn_df %>%
+      filter(x == x1) %>%
+      pull(value)
+    y2 <- basis_fn_df %>%
+      filter(x == x2) %>%
+      pull(value)
+
+    seg2_m <- (y2 - y1) / (x2 - x1)
+    seg2_c <- (x2*y1 - x1*y2) / (x2 - x1)
+
+    spline_basis_list <- append(spline_basis_list, list(list(m1 = seg1_m,
+                                                             c1 = seg1_c,
+                                                             m2 = seg2_m,
+                                                             c2 = seg2_c)))
+    names(spline_basis_list)[length(spline_basis_list)] <- as.character(basis_fn_name)
+
+  }
+
+  return(spline_basis_list)
+}
+
+
+expand_one_knot_spline_linear_predictor <- function(beta1,
+                                                    beta2,
+                                                    alpha,
+                                                    basis_functions,
+                                                    intercept = FALSE) {
+  beta_1 = beta1 * basis_functions[["1"]][["m1"]] +
+    beta2 * basis_functions[["2"]][["m1"]]
+
+  beta_2 = beta1 * basis_functions[["1"]][["m2"]] +
+    beta2 * basis_functions[["2"]][["m2"]]
+
+  regression_coefficients <- list(beta_1 = beta_1, beta_2 = beta_2)
+
+  if(intercept) {
+    alpha_1 = alpha +
+      beta1 * basis_functions[["1"]][["c1"]] +
+      beta2 * basis_functions[["2"]][["c1"]]
+
+    alpha_2 = alpha +
+      beta1 * basis_functions[["1"]][["c2"]] +
+      beta2 * basis_functions[["2"]][["c2"]]
+
+    regression_coefficients <- c(regression_coefficients,
+                                 list(alpha_1 = alpha_1,
+                                      alpha_2 = alpha_2))
+  }
+
+  return(regression_coefficients)
+
+}
+
+get_linear_combination_for_difference_in_log_rr <- function(spline_basis) {
+
+  basis_functions <- get_basis_functions_for_linear_spline_with_one_knot(spline_basis)
+
+  k1 = basis_functions[["1"]][["m2"]] - basis_functions[["1"]][["m1"]]
+  k2 = basis_functions[["2"]][["m2"]] - basis_functions[["2"]][["m1"]]
+
+  K <- matrix(c(0, k1, k2), nrow = 1L, ncol = 3L)
+
+  return(K)
+
+}
